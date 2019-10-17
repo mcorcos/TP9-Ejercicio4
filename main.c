fbac290b83d23f7368ebac95ad2674b9d3b5d736
mcorcos <mcorcos@opc-l-wrk-0001.itbadc.itba.edu.ar>
2019-10-17 13:50:44 -0300

    funciona en la primera se debe soreescribir algo

--- main.c
+++ main.c
@@ -17,52 +17,57 @@
 
 #define IS_LETTER(i)    ((i)>='a' && (i)<='z'||(i)>='A'&& (i)<='Z')
 #define CANT    50
+
 /*
  * 
  */
-int main (void)
-{
+int main(void) {
     char string[CANT];
-    char *str,*origen,*final;
-    str=string;
-    int cntinue=1,i;
-    while(cntinue){
-        
+    char *str, *origen, *final;
+    str=&string[0];
+    int cntinue = 1, i=0;
+    while (cntinue) {
+
         printf("Ingrese un String: \n");
-        origen= str;
-        final=get_str(str,string);
-        str_check(origen,final);
+        origen=str;
+        final=get_str(str, string);
+        str_check(origen, final);
         printf("Si desea salir presione Q ,de lo contrario ingrese cualquier tecla\n");
-        i=getchar();
-        if((i=='q')||(i=='Q')) {
-            cntinue=0;
-        }   
+        i = getchar();
+        if ((i == 'q') || (i == 'Q')) {
+            cntinue = 0;
+        }
     }
     return (EXIT_SUCCESS);
-    
+
 }
 
-void str_check (char * origen,char * final){
-    if (origen == final){
+void str_check(char * origen, char * final) {
+    if (origen == final) {
         printf("Es palindromo\n");
     }
-    
-    else if (*origen == *final){
-        str_check((origen-1),(final+1));
-    }
-    else {
+    else if (*origen == *final) {
+        str_check((origen+1), (final-1));
+    } else {
         printf("No es palindromo\n");
     }
 }
 
-char * get_str (char * str, char string[CANT]){
+char * get_str(char * str, char string[CANT]) {
     char pull_char;
-    int i;
-    i=0;     
-    pull_char=getchar();
-    while ( i<CANT && IS_LETTER(pull_char) && pull_char!='\n'){
-        string[i]= pull_char;
-        i++;
+    int j,i;
+    
+    for (i=0;j;) {
+        pull_char = getchar();
+        if( (pull_char=='\n')||(i==CANT) ) {
+            j=0;
+            i-=1;
+        } 
+        else if (IS_LETTER(pull_char)) {
+            string[i] = pull_char;
+            i++;
+        }
     }
-    return str[i];
+    str=&string[i];
+    return str;
 }
