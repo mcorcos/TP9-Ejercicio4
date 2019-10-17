6866ad2dc56d3b39150584616d29d0c14db048aa
mcorcos <mcorcos@opc-l-wrk-0001.itbadc.itba.edu.ar>
2019-10-17 11:40:07 -0300

    hay q arreglar este, no entiendo los errores

---
+++ recursive.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   str_check.h
+ * Author: mcorcos
+ *
+ * Created on 16 de octubre de 2019, 19:11
+ */
+
+#ifndef RECURSIVE_H
+#define RECURSIVE_H
+
+void str_check (char * origen, char * final);
+char* get_str (char * str);
+
+#endif /* RECURSIVE_H */
+
